66e1b3a471a0d28d28b8e0e2a1a33f8e6156375f
Islam Abdelrahman <islamabdelrahman@Islams-MacBook-Pro-2.local>
2021-11-21 02:17:40 +0100

    adding minimax algorithim function

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ HEIGHT = 600
 LINE_WIDTH = 15
 ROWS = 3
 COLUMNS = 3
-FIGURE_C_RADIUS=60
-FIGURE_C_WIDTH=15
+FIGURE_C_RADIUS = 60
+FIGURE_C_WIDTH = 15
 X_WiDTH = 25
-SPACE=55
+SPACE = 55
 
 # rgb :
 RED = (255, 0, 0)
-FIGURES=(224, 213, 56)
+FIGURES = (224, 213, 56)
 BACKGROUND_COLOR = (97, 97, 94)
 LINE = (46, 46, 44)
-X_COLOR=(26,26,26)
+X_COLOR = (26, 26, 26)
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('TIC TAK TOE')
@@ -53,23 +53,62 @@ def draw_lines():
 def mark(row, col, player):
     board[row][col] = player
 
-#function for ai to tick a square
+
+# function for ai to tick a square
 def aiMark():
-    highestScore= -1000
-    bestMove_X= 0
-    bestMove_Y=0
+    highestScore = -1000
+    bestMove_X = 0
+    bestMove_Y = 0
     for row in range(ROWS):
         for col in range(COLUMNS):
-            if available_spots(row,col):
-                board[row][col]= 2
-                score = minimax(board,0,False)
+            if available_spots(row, col):
+                board[row][col] = 2
+                score = minimax(board, False)
                 board[row][col] = 0
-                if(score> highestScore):
-                    highestScore=score
-                    bestMove_X=row
-                    bestMove_Y=col
-    board[bestMove_X][bestMove_Y]=2
+                if (score > highestScore):
+                    highestScore = score
+                    bestMove_X = row
+                    bestMove_Y = col
+    if available_spots(bestMove_X, bestMove_Y):
+        board[bestMove_X][bestMove_Y] = 2
     return
+
+
+# minimax algorithem function
+def minimax(board, isMaximizing):
+    if check_winner_no_drawing(1):
+        return -100
+    elif check_winner_no_drawing(2):
+        return 100
+    elif check_full_board():
+        return 0
+
+    if isMaximizing:
+        highestScore = -1000
+
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if available_spots(row, col):
+                    board[row][col] = 2
+                    score = minimax(board, False)
+                    board[row][col] = 0
+                    if (score > highestScore):
+                        highestScore = score
+        return highestScore
+
+    else:
+        highestScore = 800
+
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if available_spots(row, col):
+                    board[row][col] = 1
+                    score = minimax(board, True)
+                    board[row][col] = 0
+                    if (score < highestScore):
+                        highestScore = score
+        return highestScore
+
 
 # function to find availble spots returns true if spot is availble
 def available_spots(row, col):
@@ -78,29 +117,35 @@ def available_spots(row, col):
     else:
         return False
 
-#function to check if all fields in the board became full
+
+# function to check if all fields in the board became full
 def check_full_board():
     for row in range(ROWS):
         for col in range(COLUMNS):
-            if board[row][col] ==0:
+            if board[row][col] == 0:
                 return False
     return True
+
 
 # function to draw figures on the gui
 def draw_x_o():
     for r in range(ROWS):
         for c in range(COLUMNS):
-            if board[r][c] ==1:
-                pg.draw.circle(screen,FIGURES,(int(c*200+200/2),int(r*200+200/2)),FIGURE_C_RADIUS,FIGURE_C_WIDTH) # multiply by 200 to ge the respective corrdinates in the gui and we draw them in the center of the gui
-            elif board[r][c] ==2:
-                pg.draw.line(screen,X_COLOR,(c*200 +SPACE, r*200 + 200-SPACE),(c*200+200-SPACE,r*200+SPACE),X_WiDTH)
-                pg.draw.line(screen,X_COLOR,(c*200+SPACE,r*200+SPACE),(c*200+200-SPACE,r*200+200-SPACE),X_WiDTH)
+            if board[r][c] == 1:
+                pg.draw.circle(screen, FIGURES, (int(c * 200 + 200 / 2), int(r * 200 + 200 / 2)), FIGURE_C_RADIUS,
+                               FIGURE_C_WIDTH)  # multiply by 200 to ge the respective corrdinates in the gui and we draw them in the center of the gui
+            elif board[r][c] == 2:
+                pg.draw.line(screen, X_COLOR, (c * 200 + SPACE, r * 200 + 200 - SPACE),
+                             (c * 200 + 200 - SPACE, r * 200 + SPACE), X_WiDTH)
+                pg.draw.line(screen, X_COLOR, (c * 200 + SPACE, r * 200 + SPACE),
+                             (c * 200 + 200 - SPACE, r * 200 + 200 - SPACE), X_WiDTH)
+
 
 def check_winner(player):
     # vertical win
     for col in range(COLUMNS):
         if board[0][col] == player and board[1][col] == player and board[2][col] == player:
-            draw_vertical_end_line(col,player)
+            draw_vertical_end_line(col, player)
             return True
     # Horizontal win
     for row in range(ROWS):
@@ -108,60 +153,83 @@ def check_winner(player):
             draw_Horizontal_end_line(row, player)
             return True
     # asc diagonal win
-    if board[0][2]== player and board[1][1] == player and board[2][0] ==player:
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
         draw_asc_Diagional__line(player)
         return True
     # desc diagonal win
-    if board[0][0]== player and board[1][1] == player and board[2][2] ==player:
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
         draw_des_Diagional__line(player)
         return True
 
     return False
 
 
+def check_winner_no_drawing(player):
+    # vertical win
+    for col in range(COLUMNS):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+    # Horizontal win
+    for row in range(ROWS):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+    # asc diagonal win
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    # desc diagonal win
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
 
-#function to draw winning vertival line
-def draw_vertical_end_line(col,player):
-    postionX = col*200 +100
-    if player ==1:
+    return False
+
+
+# function to draw winning vertival line
+def draw_vertical_end_line(col, player):
+    postionX = col * 200 + 100
+    if player == 1:
         color = FIGURES
-    elif player ==2:
+    elif player == 2:
         color = X_COLOR
-    pg.draw.line(screen,color,(postionX,15),(postionX,HEIGHT-15), LINE_WIDTH)
+    pg.draw.line(screen, color, (postionX, 15), (postionX, HEIGHT - 15), LINE_WIDTH)
 
-#function to draw winning Horizontal  line
-def draw_Horizontal_end_line(row,player):
+
+# function to draw winning Horizontal  line
+def draw_Horizontal_end_line(row, player):
     postionY = row * 200 + 100
     if player == 1:
         color = FIGURES
     elif player == 2:
         color = X_COLOR
-    pg.draw.line(screen, color, (15,postionY), (WIDTH - 15,postionY), LINE_WIDTH)
+    pg.draw.line(screen, color, (15, postionY), (WIDTH - 15, postionY), LINE_WIDTH)
 
-#function to draw winning ascending diagonal line
+
+# function to draw winning ascending diagonal line
 def draw_asc_Diagional__line(player):
     if player == 1:
         color = FIGURES
     elif player == 2:
         color = X_COLOR
-    pg.draw.line(screen,color,(15,HEIGHT-15),(WIDTH-15,15),LINE_WIDTH)
+    pg.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), LINE_WIDTH)
 
-#function to draw winning descending diagonal line
+
+# function to draw winning descending diagonal line
 def draw_des_Diagional__line(player):
     if player == 1:
         color = FIGURES
     elif player == 2:
         color = X_COLOR
-    pg.draw.line(screen,color,(15,15),(WIDTH-15,HEIGHT-15),LINE_WIDTH)
+    pg.draw.line(screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), LINE_WIDTH)
 
-#function to restart the game
+
+# function to restart the game
 def restart():
     screen.fill(BACKGROUND_COLOR)
     draw_lines()
-    player=1
+    player = 1
     for row in range(ROWS):
         for col in range(COLUMNS):
-            board[row][col]=0
+            board[row][col] = 0
+
 
 draw_lines()
 player = 1
@@ -173,37 +241,33 @@ if __name__ == '__main__':
             if event.type == pg.QUIT:
                 sys.exit()
 
-            #if we click the screen we save the cordintess of the click location
+            # if we click the screen we save the cordintess of the click location
             if event.type == pg.MOUSEBUTTONDOWN and not game_over:
-                mouse_x= event.pos[0] # x_cordinates
-                mouse_y= event.pos[1] # y_cordinates
-                #CORDINTES WHERE BETWEEN 0 AND 600 AND EACH SQAURE IS 200X200 SO WEE NEED TO DIVID BY 200 to get THE x and y corrdinates in our board matrix
+                mouse_x = event.pos[0]  # x_cordinates
+                mouse_y = event.pos[1]  # y_cordinates
+                # CORDINTES WHERE BETWEEN 0 AND 600 AND EACH SQAURE IS 200X200 SO WEE NEED TO DIVID BY 200 to get THE x and y corrdinates in our board matrix
 
-                c_column= int(mouse_x // 200)
-                c_row = int(mouse_y//200)
+                c_column = int(mouse_x // 200)
+                c_row = int(mouse_y // 200)
 
-                if available_spots(c_row,c_column):
-                    if player== 1 :
-                        mark(c_row,c_column,1)
+                if available_spots(c_row, c_column):
+                    if player == 1:
+                        mark(c_row, c_column, 1)
                         if check_winner(player):
-                            game_over=True
-                        player=2
+                            game_over = True
+                        player = 2
 
-                    elif player ==2 :
-                        if available_spots(c_row, c_column):
-                            mark(c_row,c_column,2)
-                            if check_winner(player):
-                                game_over = True
-                            player=1
-
-                        player=1
+                        aiMark()
+                        if check_winner(player):
+                            game_over = True
+                        player = 1
                     draw_x_o()
                     print(board)
 
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_r:
                     restart()
-                    game_over=False
+                    game_over = False
 
         pg.display.update()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
